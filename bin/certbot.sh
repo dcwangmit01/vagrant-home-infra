124e6be8c328213109a9e2001152f0ea9a4f4781
David Wang <dcwangmit01@gmail.com>
2017-02-28 19:10:17 -0800

    Added new paxarm domain, Tweaked .emacs file

--- bin/certbot.sh
+++ bin/certbot.sh
@@ -6,13 +6,14 @@ docker run --rm \
        -v /docker/nginx/web:/var/www \
        deliverous/certbot \
        certonly \
-       --non-interactive --agree-tos \
+       --non-interactive --agree-tos --expand \
        --renew-with-new-domains \
        --rsa-key-size 4096 \
        --webroot -w /var/www \
        -m 'dcwangmit01@gmail.com' \
        -d davidwang.com -d www.davidwang.com -d s.davidwang.com -d pvr.davidwang.com \
-       -d whatismyip.davidwang.com -d registry.davidwang.com
+       -d api.davidwang.com -d registry.davidwang.com -d whatismyip.davidwang.com \
+       -d api.paxarm.com -d registry.paxarm.com -d s.paxarm.com -d www.paxarm.com
 
 # debug options
 #
